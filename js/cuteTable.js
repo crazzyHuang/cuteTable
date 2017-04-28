1e3981ceb6c0e870fe46f893527bc07c389aad36
HuangQiwei <506605454@qq.com>
2017-04-28 18:47:57 +0800

    initTable

--- js/cuteTable.js
+++ js/cuteTable.js
@@ -12,7 +12,8 @@
 	// Create the defaults once
 	var pluginName = "cuteTable",
 		defaluts = {
-
+			columns: [],
+			isShowRowNum:true
 		}
 
 	function CuteTable(element, options) {
@@ -20,7 +21,7 @@
 		this.settings = $.extend({}, defaluts, options);
 		this._defaults = defaluts;
 		this._name = pluginName;
-		this.version = 'v1.0.1';
+		this.version = 'v1.0.0';
 		this.init();
 	}
 
@@ -28,8 +29,49 @@
 		init: function() {
 			var that = this;
 			console.info("i am init");
+			// 如果是代码式
+			that.createContainer();
+
+		},
+		createContainer: function() {
+			var that = this;
+			var opts = that.settings;
+			var cols = opts.columns;
+			if(!cols) return;
+			var colHtml = '';
+			var $thead = $(that.element).find('thead');
+			var $headtr = $(that.element).find('thead tr');
+			var $th = [];
+			if(opts.isShowRowNum){
+				$th.push($('<th>/th').html("序号"));
+			}
+			for(var i = 0; i < cols.length; i++) {
+				$th.push($('<th>/th').html(cols[i].field));
+			}
+			if(!$headtr.length) {
+				$headtr = $("<tr></tr>");
+			}
+			$headtr.append($th);
+			if(!$thead.length) {
+				$thead = $("<thead></thead>");
+			}
+			$thead.append($headtr);
+			$(that.element).append($thead);
+		},
+		setData:function(){
+			var opts = that.settings;
+			var that = this;
+		 	var cols = $(that.element).find("thead td");
+			var $tr = makeTr();
+			for (var i =0;) {
+				
+			}
 		}
 	};
+	
+	function makeTr(){
+		return $("<tr></tr>");
+	}
 
 	$.fn[pluginName] = function(options) {
 		this.each(function() {
