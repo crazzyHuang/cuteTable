15ac151b3656f9dd594e945c12a7ac68ddcaf87a
HuangQiwei <506605454@qq.com>
2017-05-02 17:47:08 +0800

    renderRow

--- js/cuteTable.js
+++ js/cuteTable.js
@@ -13,7 +13,7 @@
 	var pluginName = "cuteTable",
 		defaluts = {
 			columns: [],
-			isShowRowNum:true
+			isShowRowNum: true
 		}
 
 	function CuteTable(element, options) {
@@ -31,6 +31,9 @@
 			console.info("i am init");
 			// 如果是代码式
 			that.createContainer();
+			if(that.settings.datas) {
+				that.setData();
+			}
 
 		},
 		createContainer: function() {
@@ -42,11 +45,13 @@
 			var $thead = $(that.element).find('thead');
 			var $headtr = $(that.element).find('thead tr');
 			var $th = [];
-			if(opts.isShowRowNum){
+			if(opts.isShowRowNum) {
 				$th.push($('<th>/th').html("序号"));
 			}
 			for(var i = 0; i < cols.length; i++) {
-				$th.push($('<th>/th').html(cols[i].field));
+				var $tempth = $('<th></th>').html(cols[i].field);
+				$tempth.attr("ct-name", cols[i].name);
+				$th.push($tempth);
 			}
 			if(!$headtr.length) {
 				$headtr = $("<tr></tr>");
@@ -58,18 +63,61 @@
 			$thead.append($headtr);
 			$(that.element).append($thead);
 		},
-		setData:function(){
-			var opts = that.settings;
-			var that = this;
-		 	var cols = $(that.element).find("thead td");
-			var $tr = makeTr();
-			for (var i =0;) {
-				
+		setData: function() {
+			var that = this,
+				opts = that.settings,
+				cols = $(that.element).find("thead th"); // 看看该表格的头部有什么
+
+			var fileds = [];
+			//先拿到头部的name信息
+			for(var i = 0; i < cols.length; i++) {
+				var namez = $(cols[i]).attr("ct-name");
+				if(namez){
+				fileds.push(namez);
+				}
+
 			}
+			var $allrow = [];
+			// 造数据body
+			for(var j = 0; j < opts.datas.length; j++) {
+				var $tr = makeTr();
+				var rowIndex = j + 1;
+				var $rows = this.renderRow(fileds, opts.datas[j], rowIndex);
+				$tr.append($rows);
+				$allrow.push($tr);
+			}
+
+			$(that.element).append($allrow);
+
+		},
+		renderRow: function(fileds, row, rowIndex) {
+			var rs = [],that = this,opt = that.settings;
+
+			for(var i = 0; i < fileds.length; i++) {
+				if(row[fileds[i]]) { // 如果存在该字段
+					if(opt.isShowRowNum) { // 序号
+						rs.push("<td>");
+						rs.push(rowIndex);
+						rs.push("</td>");
+					}
+					rs.push('<td filed="' + row[fileds[i]] + '">');
+					rs.push(row[fileds[i]]);
+					rs.push('</td>');
+				} else {
+					if(opt.isShowRowNum) { // 序号
+						rs.push("<td>");
+						rs.push(rowIndex);
+						rs.push("</td>");
+					}
+					rs.push("<td>"); // 置空
+					rs.push("</td>");
+				}
+			}
+			return rs.join("");
 		}
 	};
-	
-	function makeTr(){
+
+	function makeTr() {
 		return $("<tr></tr>");
 	}
 
